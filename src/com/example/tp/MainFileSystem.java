ab970983839f473f783b278f10387c16e7d7d828
nahmame0504-beep <n.ahmame0504@uca.ac.ma>
2026-09-13 16:30:08 +0100

    Ajout des fichiers du TP7

---
+++ src/com/example/tp/MainFileSystem.java
@@ -0,0 +1,28 @@
+package com.example.tp;
+
+public class MainFileSystem {
+    public static void main(String[] args) {
+        FileSystem fs = new FileSystem();
+
+        // Création de fichiers
+        FileItem f1 = new FileItem("doc.txt", 1200);
+        FileItem f2 = new FileItem("image.png", 450000);
+        FileItem f3 = new FileItem("notes.pdf", 80000);
+
+        // Répertoires
+        Directory home = new Directory("home");
+        home.add(f1);
+        home.add(f2);
+
+        Directory docs = new Directory("docs");
+        docs.add(f3);
+        home.add(docs);
+
+        fs.addRoot(home);
+        fs.addRoot(new FileItem("readme.md", 3000));
+
+        // Affichage
+        fs.listAll();
+        System.out.println("Taille totale : " + fs.totalSize() + " B");
+    }
+}
